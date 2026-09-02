16ccfd38c5bc9e8ea2815dc5bd8703b378b5f3ef
Yatharth Chandel <yatharth.chandel.15012007@gmail.com>
2026-09-02 06:21:44 +0530

    Phase 5 complete: add loki-stack for log aggregation alongside kube-prometheus-stack

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -3,8 +3,15 @@ resource "helm_release" "kube_prometheus_stack" {
 	name = "kube-prometheus-stack"
 	repository = "https://prometheus-community.github.io/helm-charts"
 	chart = "kube-prometheus-stack"
-namespace = "monitoring"
-version = "87.16.1"
+	namespace = "monitoring"
+	version = "87.16.1"
 	create_namespace = true
 }
 	
+resource "helm_release" "loki_stack" {
+	name = "loki-stack"
+	repository = "https://grafana.github.io/helm-charts"
+	chart = "loki-stack"
+	version = "2.10.3"
+	namespace = "monitoring"
+}
